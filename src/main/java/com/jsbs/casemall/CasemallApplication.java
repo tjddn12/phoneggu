c51818eb8f564419dacb0f4f5cfe9bb58702b0e3
sungwoo <hwang55285@naver.com>
2024-05-16 11:14:40 +0900

    초기설정

---
+++ src/main/java/com/jsbs/casemall/CasemallApplication.java
@@ -0,0 +1,13 @@
+package com.jsbs.casemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasemallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasemallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsbs/casemall/CasemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsbs.casemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasemallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
