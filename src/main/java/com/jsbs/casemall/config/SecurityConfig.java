7737a84277533b28c2382c0e77b021f9b23fa965
tjddn12 <hwang55285@naver.com>
2024-05-16 23:21:40 +0900

    관리자 권한 메소드 테스트 구현

--- src/main/java/com/jsbs/casemall/config/SecurityConfig.java
+++ src/main/java/com/jsbs/casemall/config/SecurityConfig.java
@@ -1,26 +1,52 @@
 package com.jsbs.casemall.config;
 
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
 
     // 시큐리티 필터 체인
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/api1").hasRole("user")
+                                .requestMatchers("/api2").hasRole("admin")
+                                .anyRequest().authenticated()
+                )
+                .formLogin((user) ->
+                        user
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true));
 
-
-        return  http.build();
+        return http.build();
     }
 
+    // 관리자 권한 부여 메소드
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user1").password("1234").roles("admin").build());
+        return manager;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
