e75c5af0bca22596bbcef0c84020847f680e1e28
KangBaekCheon <161662093+KangBaekCheon@users.noreply.github.com>
2024-05-23 09:48:53 +0900

    임시파일 수정1

---
+++ src/main/java/com/jsbs/casemall/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.jsbs.casemall.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${uploadPath}")
+    String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                //1.웹 브라우저에 입력하는 url에 /imgage 로 시작하는 경우
+                //uploadPath에 설정한 폴더를 기준으로 파일을 읽어오도록 설정
+                .addResourceLocations(uploadPath);
+                //2.로컬 컴퓨터에 저장된 파일을 읽어올 root 경로를 설정
+
+
+    }
+
+}
