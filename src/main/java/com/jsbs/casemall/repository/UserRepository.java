8d0f507fed6816d27009771a54496d9853a8b44c
0wvgruh <ksh02040778@gmail.com>
2024-06-12 14:50:12 +0900

    회원가입 수정3 푸시완료

--- src/main/java/com/jsbs/casemall/repository/UserRepository.java
+++ src/main/java/com/jsbs/casemall/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, String> {
+
     Users findByEmail(String email);
 
     Optional<Users> findByEmailAndPhone(String email, String phone);
