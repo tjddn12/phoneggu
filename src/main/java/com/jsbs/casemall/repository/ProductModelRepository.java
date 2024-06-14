f726c64ed3cfdfbafa8c136dbeb827fc83fb0f15
KangBaekCheon <161662093+KangBaekCheon@users.noreply.github.com>
2024-06-14 11:33:05 +0900

    모델 null값 없이 저장

--- src/main/java/com/jsbs/casemall/repository/ProductModelRepository.java
+++ src/main/java/com/jsbs/casemall/repository/ProductModelRepository.java
@@ -2,6 +2,7 @@ package com.jsbs.casemall.repository;
 
 import com.jsbs.casemall.entity.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,4 +12,8 @@ public interface ProductModelRepository extends JpaRepository<ProductModel, Long
 
     @Query("SELECT pm FROM ProductModel pm WHERE pm.product.id = :productId")
     List<ProductModel> findByProductId(@Param("productId") Long productId);
+
+    @Modifying
+    @Query("DELETE FROM ProductModel pm WHERE pm.product.id IS NULL")
+    void deleteByPrIdIsNull();
 }
