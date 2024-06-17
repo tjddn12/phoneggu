b092069029bbf285d4bec6938fd39431e7005942
0wvgruh <ksh02040778@gmail.com>
2024-06-17 17:47:43 +0900

    회원가입 수정6/17 푸시완료 3

--- src/main/java/com/jsbs/casemall/config/SecurityConfig.java
+++ src/main/java/com/jsbs/casemall/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.jsbs.casemall.config;
 
-import com.jsbs.casemall.service.PrincipalOauth2UserService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,41 +12,50 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
 
+    // 인증 인가 부여
     @Bean
-    public SecurityFilterChain config(HttpSecurity http, PrincipalOauth2UserService principalOauth2UserService) throws Exception {
+    public SecurityFilterChain config(HttpSecurity http) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/user/**", "/order/**", "/pay/**").authenticated()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                // 요청에 따른 인가 설정
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/user/**", "/order/**","/pay/**").authenticated() // 로그인성공한 사람만  user /밑에 모든  경로에 접근 가능 예를 들면 마이페이지 구매 진행 이런거
+                        .requestMatchers("/admin/**") // 관리자로 저장된 회원만 admin / 아래 모든 모든곳에 접근 가능
+                        .hasRole("ADMIN")
                         .requestMatchers("/test/**").authenticated()
-                        .anyRequest().permitAll()
+                        .anyRequest().permitAll() // 그 외에 는 접근 허용
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/", true)
                 )
                 .formLogin(login -> login
                         .usernameParameter("userId")
                         .passwordParameter("userPw")
                         .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(principalOauth2UserService)
-                        )
-                )
+                        .defaultSuccessUrl("/"))
+
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                );
+                        .logoutSuccessUrl("/") // 로그아웃 성공시 메인페이지로 갈수있게
+                        .invalidateHttpSession(true)) // 세션에 정보 삭제
+//                .sessionManagement(session -> session
+//                        .invalidSessionUrl("/login?invalid-session=true"))
+        ;
+
 
         return http.build();
     }
 
+
+    // 비밀번호 암호화
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
