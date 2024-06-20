f9a8572941b48d1cce02289b2af34a9fe2cd7a77
sungwoo <hwang55285@naver.com>
2024-06-20 17:01:11 +0900

    단순 커밋

--- src/main/java/com/jsbs/casemall/entity/Users.java
+++ src/main/java/com/jsbs/casemall/entity/Users.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @NoArgsConstructor
 public class Users {
 
-
+    private String username;
 
     @Id
     @Column(name = "userId")
